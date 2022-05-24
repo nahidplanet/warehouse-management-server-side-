2bf691fa8e99f56e66176587da8e791c74084aa8
nahidplanet <nahidplanet.bd@gmail.com>
2022-05-24 20:12:56 +0600

    finish

--- index.js
+++ index.js
@@ -13,54 +13,54 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lgtub.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run = async () => {
-  try{
+  try {
     await client.connect();
     const productCollections = client.db("rayz").collection("product");
 
     // PRODUCT 
-    app.get('/product',async (req,res)=>{
+    app.get('/product', async (req, res) => {
       const query = {};
       const cursor = productCollections.find(query);
       const products = await cursor.toArray();
       res.send(products);
     });
     // PRODUCT DETAILS BY ID 
-    app.get('/product/:id',async (req,res)=>{
+    app.get('/product/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const product = await productCollections.findOne(query);
       res.send(product);
     });
     // ADD PRODUCT ;
-    app.post('/product',async (req,res) => {
+    app.post('/product', async (req, res) => {
       const newProduct = req.body;
       const result = await productCollections.insertOne(newProduct);
       res.send(result);
     });
     // delete product 
-    app.delete('/product/:id' , async (req,res)=>{
+    app.delete('/product/:id', async (req, res) => {
       const deletedId = req.params.id;
-      const query = {_id: ObjectId(deletedId)};
+      const query = { _id: ObjectId(deletedId) };
       const result = await productCollections.deleteOne(query);
       res.send(result);
     });
     // UPDATE QUANTITY 
-    app.put('/product',async (req,res)=>{
+    app.put('/product', async (req, res) => {
       const id = req.query.updateid;
       const value = req.query.value;
-      const query = {_id:ObjectId(id)}
-      const option = {upsert:true};
+      const query = { _id: ObjectId(id) }
+      const option = { upsert: true };
       const updeteValue = {
-        $set:{
-          quantity:value
+        $set: {
+          quantity: value
         }
       };
-      const result = await productCollections.updateOne(query,updeteValue,option);
+      const result = await productCollections.updateOne(query, updeteValue, option);
       res.send(result);
 
     })
     //MY PRODUCT SHOW 
-    app.get('/myproduct',async(req,res)=>{
+    app.get('/myproduct', async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const cursor = productCollections.find(query);
@@ -68,19 +68,15 @@ const run = async () => {
       res.json(result);
     })
 
-
-
-  }finally{
+  } finally {
     // client.close();
   }
-
 }
 run().catch(console.dir);
 
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send("Rayz API Home");
 });
-app.listen(port,()=>{
-  console.log("Rayz Server Is Running",port);
-})
+app.listen(port, () => {
+  console.log("Rayz Server Is Running", port);
+});
